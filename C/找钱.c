f997105b7dd050c2718fb226cb660dd0bf22dbd1
joker-kali38 <3403788281@qq.com>
2025-11-11 11:28:12 +0800

    更新C学习文档

--- C/找钱.c
+++ C/找钱.c
@@ -1,10 +1,16 @@
 #include<stdio.h>
 int main()
 {
-	int price;
+	int price,bill;
 	printf("请输入金额：");
 	scanf("%d",&price);
-	int change=100-price;
-	printf("应找回：%d",change);
+	printf("请输入票面：");
+	scanf("%d",&bill);
+	if(bill>=price){
+		printf("应找回：%d",bill-price);
+	}
+	else{
+		printf("还需要：%d",price-bill);
+	}
 	return 0;
 }
--- C/计算时间差.c
+++ C/计算时间差.c
@@ -5,8 +5,13 @@ int main()
 	int hour2,minute2;
 	scanf("%d %d",&hour1,&minute1);
 	scanf("%d %d",&hour2,&minute2);
-	int t1=hour1*60+minute1;
-	int t2=hour2*60+minute2;
-	int t=t2-t1;
-	printf("%d %d",t/60,t%60);
+	int ih=hour2-hour1;
+	int im=minute2-minute1;
+	if(im<0)
+	{
+		im=60+im;
+		ih--;
+	}
+	printf("时差是%d小时%d分钟",ih,im);
+	return 0;
 }
