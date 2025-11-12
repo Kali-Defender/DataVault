9fa28ce75a6941beace5b7f6095510589769f534
joker-kali38 <3403788281@qq.com>
2025-11-12 17:46:16 +0800

    更新Python学习文档

--- Python/值得收藏的30道Python练手题.py
+++ Python/值得收藏的30道Python练手题.py
@@ -72,11 +72,83 @@
 # print(test("gbgkkdehh",1,2))
 
 #8. 判断字符串 a = “welcome to my world” 是否包含单词 b = “world”，包含返回 True，不包含返回 False。
-def test():
-    test="Hello, welcome to my world."
-    b="welcome"
-    if b in test:
-        return True
-    else:
-        return False
-print(test())
+# def test():
+#     test="Hello, welcome to my world."
+#     b="welcome"
+#     if b in test:
+#         return True
+#     else:
+#         return False
+# print(test())
+
+#9. 从 0 开始计数，输出指定字符串 A = “hello” 在字符串 B = “hi how are you hello world, hello yoyo!”中第一次出现的位置，如果 B 中不包含 A，则输出 -1。
+# def test():
+#     test="hi how are you hello world, hello yoyo!"
+#     b="hello"
+#     if b in test:
+#         return test.find(b)
+#     else:
+#         return -1
+# print(test())
+
+#10. 从 0 开始计数，输出指定字符串 A = “hello”在字符串 B = “hi how are you hello world, hello yoyo!”中最后出现的位置，如果 B 中不包含 A，则输出 -1。
+# def test(string,str):
+#     last_position=-1
+#     while True:
+#         position=string.find(str,last_position+1)
+#         if position==-1:
+#             return last_position
+#         last_position=position
+
+
+# print(test("hi how are you hello world, hello yoyo","hello"))
+
+# s = "hi how are you hello world, hello yoyo!"
+# print(s.rfind("hello"))  # 也是输出 29
+
+#11. 给定一个数 a，判断一个数字是否为奇数或偶数。
+# num=int(input("请输入一个整数"))
+# if num%2==0:
+#     print("偶数")
+# else:
+#     print("奇数")
+
+#12. 输入一个姓名，判断是否姓王。
+# def test():
+#     user_input=input("请输入你的姓名：")
+#     if user_input[0]=="王":
+#         return "用户姓王"
+#     return "用户不姓王"
+# print(test())
+
+#13.如何判断一个字符串是不是纯数字组成？
+# def test(num):
+#     try:
+#         return float(num)
+#     except ValueError:
+#         return "请输入数字"
+# print(test("233f4"))
+#异常处理机制，防止程序崩溃
+
+#14. 将字符串 a = “This is string example….wow!” 全部转成大写，字符串 b = “Welcome To My World” 全部转成小写。
+# a="This is string example...wow!"
+# b="Welcome To My World"
+# print(a.upper())
+# print(b.lower())
+
+#15. 将字符串 a = “ welcome to my world ”首尾空格去掉
+# test=" asdf "
+# print(test.strip())
+# Python 提供了strip() 方法，可以去除首尾空格，rstrip() 去掉尾部空格，lstrip() 去掉首部空格，replace(" ", “”) 去掉全部空格。
+
+#16. 将字符串 s = “ajldjlajfdljfddd”，去重并从小到大排序输出”adfjl”。
+# def test():
+#     s="ajldjlajfdljfddd"
+#     str_list=[]
+#     for i in s:
+#         if i in str_list:
+#             str_list.remove(i)
+#         str_list.append(i)
+#     a=sorted(str_list)
+#     return "".join(a)
+# print(test())
