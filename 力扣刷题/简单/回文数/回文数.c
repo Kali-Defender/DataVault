96e97024264dfca2b87123dec3681572820f16c5
joker-kali38 <3403788281@qq.com>
2025-11-17 18:47:12 +0800

    添加力扣刷题文档

---
+++ 力扣刷题/简单/回文数/回文数.c
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x<0&&x%10==0) 
+        return false;
+        int r=x;
+        long long t=0;
+        while(x>0){
+            t=t*10+x%10;
+		    x/=10;
+        }
+        if(t==r){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
